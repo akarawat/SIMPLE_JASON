8cd6d93698fb433aff65223889b220de7d16836e
Akarawat panwilai <akarawat.bis@gmail.com>
2018-11-13 16:06:26 +0700

    Initial commit

---
+++ SIMPLE_JASON/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SIMPLE_JASON
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtJsonRes = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.chkTxt = new System.Windows.Forms.CheckBox();
+            this.chkDebug = new System.Windows.Forms.CheckBox();
+            this.btnJson = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnPut = new System.Windows.Forms.Button();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtJsonRes
+            // 
+            this.txtJsonRes.Location = new System.Drawing.Point(12, 67);
+            this.txtJsonRes.Name = "txtJsonRes";
+            this.txtJsonRes.Size = new System.Drawing.Size(710, 253);
+            this.txtJsonRes.TabIndex = 29;
+            this.txtJsonRes.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label8.Location = new System.Drawing.Point(19, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 17);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "URL :";
+            // 
+            // chkTxt
+            // 
+            this.chkTxt.AutoSize = true;
+            this.chkTxt.Location = new System.Drawing.Point(462, 20);
+            this.chkTxt.Name = "chkTxt";
+            this.chkTxt.Size = new System.Drawing.Size(132, 17);
+            this.chkTxt.TabIndex = 27;
+            this.chkTxt.Text = "Debug json to text box";
+            this.chkTxt.UseVisualStyleBackColor = true;
+            // 
+            // chkDebug
+            // 
+            this.chkDebug.AutoSize = true;
+            this.chkDebug.Location = new System.Drawing.Point(614, 20);
+            this.chkDebug.Name = "chkDebug";
+            this.chkDebug.Size = new System.Drawing.Size(108, 17);
+            this.chkDebug.TabIndex = 26;
+            this.chkDebug.Text = "Debug json to file";
+            this.chkDebug.UseVisualStyleBackColor = true;
+            // 
+            // btnJson
+            // 
+            this.btnJson.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJson.Location = new System.Drawing.Point(382, 16);
+            this.btnJson.Name = "btnJson";
+            this.btnJson.Size = new System.Drawing.Size(74, 23);
+            this.btnJson.TabIndex = 25;
+            this.btnJson.Text = "Get";
+            this.btnJson.UseVisualStyleBackColor = true;
+            this.btnJson.Click += new System.EventHandler(this.btnJson_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtUrl.Location = new System.Drawing.Point(69, 16);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(307, 23);
+            this.txtUrl.TabIndex = 24;
+            this.txtUrl.Text = "http://172.21.42.114:9200/uiddb/uidall/_count";
+            // 
+            // btnPut
+            // 
+            this.btnPut.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPut.Location = new System.Drawing.Point(648, 552);
+            this.btnPut.Name = "btnPut";
+            this.btnPut.Size = new System.Drawing.Size(74, 23);
+            this.btnPut.TabIndex = 25;
+            this.btnPut.Text = "PUT";
+            this.btnPut.UseVisualStyleBackColor = true;
+            this.btnPut.Click += new System.EventHandler(this.btnPut_Click);
+            // 
+            // btnPost
+            // 
+            this.btnPost.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPost.Location = new System.Drawing.Point(568, 552);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(74, 23);
+            this.btnPost.TabIndex = 25;
+            this.btnPost.Text = "POST";
+            this.btnPost.UseVisualStyleBackColor = true;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 353);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(710, 193);
+            this.richTextBox1.TabIndex = 30;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(19, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Result :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(19, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Command :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 583);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtJsonRes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.chkTxt);
+            this.Controls.Add(this.chkDebug);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.btnPut);
+            this.Controls.Add(this.btnJson);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtJsonRes;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox chkTxt;
+        private System.Windows.Forms.CheckBox chkDebug;
+        private System.Windows.Forms.Button btnJson;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnPut;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SIMPLE_JASON/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+
+namespace SIMPLE_JASON
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJson_Click(object sender, EventArgs e)
+        {
+            //return;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(txtUrl.Text);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            //using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            //{
+            //    string json = tXml.Trim();
+            //    streamWriter.Write(json);
+            //    streamWriter.Flush();
+            //    streamWriter.Close();
+            //}
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                if (chkDebug.Checked)
+                {
+                    if (File.Exists("DebugJson.txt") && chkDebug.Checked)
+                    {
+                        File.WriteAllText("DebugJson.txt", String.Empty);
+                    }
+                    if (File.Exists("DebugJson.txt") && chkDebug.Checked)
+                    {
+                        StreamWriter w = File.AppendText("DebugJson.txt");
+                        w.WriteLine(result);
+                        w.Flush();
+                        w.Close();
+                    }
+                }
+                if (chkTxt.Checked) txtJsonRes.Text = result;
+            }
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            DumpToElastic(txtUrl.Text, richTextBox1.Text, "POST");
+        }
+        private void DumpToElastic(string tUrl, string tXml, string method)
+        {
+            //return;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(tUrl);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = method;
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = tXml.Trim();
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                txtJsonRes.Text = result;
+            }
+        }
+
+        private void btnPut_Click(object sender, EventArgs e)
+        {
+            DumpToElastic(txtUrl.Text, richTextBox1.Text, "PUT");
+        }
+    }
+}
